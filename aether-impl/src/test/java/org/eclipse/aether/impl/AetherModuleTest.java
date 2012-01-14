e4eace696980bf7b5b5648c6ac26a54a0504611b
Benjamin Bentmann <bentmann@sonatype.com>
2012-01-14 21:37:38 +0100

    Added AetherModuleTest to check completeness of provides Guice bindings

---
+++ aether-impl/src/test/java/org/eclipse/aether/impl/AetherModuleTest.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Sonatype, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Sonatype, Inc. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.aether.impl;
+
+import static org.junit.Assert.*;
+
+import java.util.Collections;
+import java.util.Set;
+
+import org.eclipse.aether.RepositorySystem;
+import org.eclipse.aether.spi.connector.RepositoryConnectorFactory;
+import org.junit.Test;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Provides;
+
+public class AetherModuleTest
+{
+
+    @Test
+    public void testModuleCompleteness()
+    {
+        assertNotNull( Guice.createInjector( new SystemModule() ).getInstance( RepositorySystem.class ) );
+    }
+
+    static class SystemModule
+        extends AbstractModule
+    {
+
+        @Override
+        protected void configure()
+        {
+            install( new AetherModule() );
+            bind( ArtifactDescriptorReader.class ).to( StubArtifactDescriptorReader.class );
+            bind( VersionRangeResolver.class ).to( StubVersionRangeResolver.class );
+            bind( VersionResolver.class ).to( StubVersionResolver.class );
+        }
+
+        @Provides
+        public Set<MetadataGeneratorFactory> metadataGeneratorFactories()
+        {
+            return Collections.emptySet();
+        }
+
+        @Provides
+        public Set<RepositoryConnectorFactory> repositoryConnectorFactories()
+        {
+            return Collections.emptySet();
+        }
+
+    }
+
+}
