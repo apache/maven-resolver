51668b4e206834ade8186c1d75b772a176e4efb9
Benjamin Bentmann <bentmann@sonatype.com>
2012-09-17 00:15:25 +0200

    Made DefaultAuthenticationSelector.add() accept null authentication to ease usage

--- aether-util/src/main/java/org/eclipse/aether/util/repository/DefaultAuthenticationSelector.java
+++ aether-util/src/main/java/org/eclipse/aether/util/repository/DefaultAuthenticationSelector.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2011 Sonatype, Inc.
+ * Copyright (c) 2010, 2012 Sonatype, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -31,12 +31,19 @@ public final class DefaultAuthenticationSelector
      * Adds the specified authentication info for the given repository identifier.
      * 
      * @param id The identifier of the repository to add the authentication for, must not be {@code null}.
-     * @param auth The authentication to add, must not be {@code null}.
+     * @param auth The authentication to add, may be {@code null}.
      * @return This selector for chaining, never {@code null}.
      */
     public DefaultAuthenticationSelector add( String id, Authentication auth )
     {
-        repos.put( id, auth );
+        if ( auth != null )
+        {
+            repos.put( id, auth );
+        }
+        else
+        {
+            repos.remove( id );
+        }
 
         return this;
     }
