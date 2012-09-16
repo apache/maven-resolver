0b07c36f0be738496735abdcf06a77f8919ae461
Benjamin Bentmann <bentmann@sonatype.com>
2012-09-17 00:17:52 +0200

    Removed superfluous Proxy.set*() methods to simplify API

--- aether-api/src/main/java/org/eclipse/aether/repository/Proxy.java
+++ aether-api/src/main/java/org/eclipse/aether/repository/Proxy.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2011 Sonatype, Inc.
+ * Copyright (c) 2010, 2012 Sonatype, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,8 +11,7 @@
 package org.eclipse.aether.repository;
 
 /**
- * A proxy to use for connections to a repository. <em>Note:</em> Instances of this class are immutable and the exposed
- * mutators return new objects rather than changing the current instance.
+ * A proxy to use for connections to a repository.
  */
 public final class Proxy
 {
@@ -62,21 +61,6 @@ public final class Proxy
     }
 
     /**
-     * Sets the type of the proxy.
-     * 
-     * @param type The type of the proxy, e.g. "http", may be {@code null}.
-     * @return The new proxy, never {@code null}.
-     */
-    public Proxy setType( String type )
-    {
-        if ( this.type.equals( type ) || ( type == null && this.type.length() <= 0 ) )
-        {
-            return this;
-        }
-        return new Proxy( type, host, port, auth );
-    }
-
-    /**
      * Gets the host for this proxy.
      * 
      * @return The host for this proxy, never {@code null}.
@@ -84,21 +68,6 @@ public final class Proxy
     public String getHost()
     {
         return host;
-    }
-
-    /**
-     * Sets the host of the proxy.
-     * 
-     * @param host The host of the proxy, may be {@code null}.
-     * @return The new proxy, never {@code null}.
-     */
-    public Proxy setHost( String host )
-    {
-        if ( this.host.equals( host ) || ( host == null && this.host.length() <= 0 ) )
-        {
-            return this;
-        }
-        return new Proxy( type, host, port, auth );
     }
 
     /**
@@ -112,21 +81,6 @@ public final class Proxy
     }
 
     /**
-     * Sets the port number for the proxy.
-     * 
-     * @param port The port number for the proxy.
-     * @return The new proxy, never {@code null}.
-     */
-    public Proxy setPort( int port )
-    {
-        if ( this.port == port )
-        {
-            return this;
-        }
-        return new Proxy( type, host, port, auth );
-    }
-
-    /**
      * Gets the authentication to use for the proxy connection.
      * 
      * @return The authentication to use or {@code null} if none.
@@ -134,21 +88,6 @@ public final class Proxy
     public Authentication getAuthentication()
     {
         return auth;
-    }
-
-    /**
-     * Sets the authentication to use for the proxy connection.
-     * 
-     * @param auth The authentication to use, may be {@code null}.
-     * @return The new proxy, never {@code null}.
-     */
-    public Proxy setAuthentication( Authentication auth )
-    {
-        if ( eq( this.auth, auth ) )
-        {
-            return this;
-        }
-        return new Proxy( type, host, port, auth );
     }
 
     @Override
