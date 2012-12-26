2c49c4ee33c5dfaf43543ae6c1c129120a48bce9
Benjamin Bentmann <bentmann@sonatype.com>
2012-12-26 18:09:14 +0100

    Fixed StubVersionRangeResolver to provide parsed version constraints for dependency nodes

--- aether-impl/src/test/java/org/eclipse/aether/internal/impl/StubVersionRangeResolver.java
+++ aether-impl/src/test/java/org/eclipse/aether/internal/impl/StubVersionRangeResolver.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2011 Sonatype, Inc.
+ * Copyright (c) 2010, 2012 Sonatype, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,17 +14,21 @@ import java.util.Arrays;
 
 import org.eclipse.aether.RepositorySystemSession;
 import org.eclipse.aether.impl.VersionRangeResolver;
-import org.eclipse.aether.internal.test.util.impl.StubVersion;
+import org.eclipse.aether.internal.test.util.impl.TestVersionScheme;
 import org.eclipse.aether.resolution.VersionRangeRequest;
 import org.eclipse.aether.resolution.VersionRangeResolutionException;
 import org.eclipse.aether.resolution.VersionRangeResult;
+import org.eclipse.aether.version.InvalidVersionSpecificationException;
 import org.eclipse.aether.version.Version;
+import org.eclipse.aether.version.VersionScheme;
 
 /**
  */
 public class StubVersionRangeResolver
     implements VersionRangeResolver
 {
+
+    private final VersionScheme versionScheme = new TestVersionScheme();
 
     public VersionRangeResult resolveVersionRange( RepositorySystemSession session, VersionRangeRequest request )
         throws VersionRangeResolutionException
@@ -39,11 +43,20 @@ public class StubVersionRangeResolver
         }
 
         VersionRangeResult result = new VersionRangeResult( request );
-        StubVersion ver = new StubVersion( version );
-        result.setVersions( Arrays.asList( (Version) ver ) );
-        if ( range && !request.getRepositories().isEmpty() )
+        try
         {
-            result.setRepository( ver, request.getRepositories().get( 0 ) );
+            Version ver = versionScheme.parseVersion( version );
+            result.setVersionConstraint( versionScheme.parseVersionConstraint( request.getArtifact().getVersion() ) );
+            result.setVersions( Arrays.asList( (Version) ver ) );
+            if ( range && !request.getRepositories().isEmpty() )
+            {
+                result.setRepository( ver, request.getRepositories().get( 0 ) );
+            }
+        }
+        catch ( InvalidVersionSpecificationException e )
+        {
+            result.addException( e );
+            throw new VersionRangeResolutionException( result );
         }
 
         return result;
--- aether-test-util/src/main/java/org/eclipse/aether/internal/test/util/impl/StubVersion.java
+++ aether-test-util/src/main/java/org/eclipse/aether/internal/test/util/impl/StubVersion.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2011 Sonatype, Inc.
+ * Copyright (c) 2010, 2012 Sonatype, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -15,7 +15,7 @@ import org.eclipse.aether.version.Version;
 /**
  * Version ordering by {@link String#compareToIgnoreCase(String)}.
  */
-public final class StubVersion
+final class StubVersion
     implements Version
 {
 
