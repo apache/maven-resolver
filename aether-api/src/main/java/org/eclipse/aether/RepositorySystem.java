d692d3dba6c0eece6e25af4856111d5cc58ab2b8
Benjamin Bentmann <bentmann@sonatype.com>
2013-12-08 14:21:47 +0100

    Extended API docs for resolveVersionRange()

--- aether-api/src/main/java/org/eclipse/aether/RepositorySystem.java
+++ aether-api/src/main/java/org/eclipse/aether/RepositorySystem.java
@@ -56,9 +56,12 @@ public interface RepositorySystem
 
     /**
      * Expands a version range to a list of matching versions, in ascending order. For example, resolves "[3.8,4.0)" to
-     * ["3.8", "3.8.1", "3.8.2"]. Note that the returned list of versions is only dependent on the configured
-     * repositories and their contents, the list is not processed by the
-     * {@link RepositorySystemSession#getVersionFilter() session's version filter}.
+     * "3.8", "3.8.1", "3.8.2". Note that the returned list of versions is only dependent on the configured repositories
+     * and their contents, the list is not processed by the {@link RepositorySystemSession#getVersionFilter() session's
+     * version filter}.
+     * <p>
+     * The supplied request may also refer to a single concrete version rather than a version range. In this case
+     * though, the result contains simply the (parsed) input version, regardless of the repositories and their contents.
      * 
      * @param session The repository session, must not be {@code null}.
      * @param request The version range request, must not be {@code null}.
