29db77b8e0db82a2ac22c90dbda69279ae8330f6
Benjamin Bentmann <bentmann@sonatype.com>
2013-12-30 21:44:54 +0100

    Improved API docs for ConfigUtils

--- aether-util/src/main/java/org/eclipse/aether/util/ConfigUtils.java
+++ aether-util/src/main/java/org/eclipse/aether/util/ConfigUtils.java
@@ -35,9 +35,9 @@ public final class ConfigUtils
      * Gets the specified configuration property.
      * 
      * @param properties The configuration properties to read, must not be {@code null}.
-     * @param defaultValue The default value to return in case the property isn't set, may be {@code null}.
-     * @param keys The properties to read, must not be {@code null}. The specified keys are read one after one until a
-     *            valid value is found.
+     * @param defaultValue The default value to return in case none of the property keys are set, may be {@code null}.
+     * @param keys The property keys to read, must not be {@code null}. The specified keys are read one after one until
+     *            a valid value is found.
      * @return The property value or {@code null} if none.
      */
     public static Object getObject( Map<?, ?> properties, Object defaultValue, String... keys )
@@ -60,9 +60,9 @@ public final class ConfigUtils
      * 
      * @param session The repository system session from which to read the configuration property, must not be
      *            {@code null}.
-     * @param defaultValue The default value to return in case the property isn't set, may be {@code null}.
-     * @param keys The properties to read, must not be {@code null}. The specified keys are read one after one until a
-     *            valid value is found.
+     * @param defaultValue The default value to return in case none of the property keys are set, may be {@code null}.
+     * @param keys The property keys to read, must not be {@code null}. The specified keys are read one after one until
+     *            a valid value is found.
      * @return The property value or {@code null} if none.
      */
     public static Object getObject( RepositorySystemSession session, Object defaultValue, String... keys )
@@ -74,9 +74,10 @@ public final class ConfigUtils
      * Gets the specified configuration property.
      * 
      * @param properties The configuration properties to read, must not be {@code null}.
-     * @param defaultValue The default value to return in case the property isn't set, may be {@code null}.
-     * @param keys The properties to read, must not be {@code null}. The specified keys are read one after one until a
-     *            valid value is found.
+     * @param defaultValue The default value to return in case none of the property keys is set to a string, may be
+     *            {@code null}.
+     * @param keys The property keys to read, must not be {@code null}. The specified keys are read one after one until
+     *            a string value is found.
      * @return The property value or {@code null} if none.
      */
     public static String getString( Map<?, ?> properties, String defaultValue, String... keys )
@@ -99,9 +100,10 @@ public final class ConfigUtils
      * 
      * @param session The repository system session from which to read the configuration property, must not be
      *            {@code null}.
-     * @param defaultValue The default value to return in case the property isn't set, may be {@code null}.
-     * @param keys The properties to read, must not be {@code null}. The specified keys are read one after one until a
-     *            valid value is found.
+     * @param defaultValue The default value to return in case none of the property keys is set to a string, may be
+     *            {@code null}.
+     * @param keys The property keys to read, must not be {@code null}. The specified keys are read one after one until
+     *            a string value is found.
      * @return The property value or {@code null} if none.
      */
     public static String getString( RepositorySystemSession session, String defaultValue, String... keys )
@@ -113,9 +115,9 @@ public final class ConfigUtils
      * Gets the specified configuration property.
      * 
      * @param properties The configuration properties to read, must not be {@code null}.
-     * @param defaultValue The default value to return in case the property isn't set.
-     * @param keys The properties to read, must not be {@code null}. The specified keys are read one after one until a
-     *            valid value is found.
+     * @param defaultValue The default value to return in case none of the property keys is set to a number.
+     * @param keys The property keys to read, must not be {@code null}. The specified keys are read one after one until
+     *            a {@link Number} or a string representation of an {@link Integer} is found.
      * @return The property value.
      */
     public static int getInteger( Map<?, ?> properties, int defaultValue, String... keys )
@@ -147,9 +149,9 @@ public final class ConfigUtils
      * 
      * @param session The repository system session from which to read the configuration property, must not be
      *            {@code null}.
-     * @param defaultValue The default value to return in case the property isn't set.
-     * @param keys The properties to read, must not be {@code null}. The specified keys are read one after one until a
-     *            valid value is found.
+     * @param defaultValue The default value to return in case none of the property keys is set to a number.
+     * @param keys The property keys to read, must not be {@code null}. The specified keys are read one after one until
+     *            a {@link Number} or a string representation of an {@link Integer} is found.
      * @return The property value.
      */
     public static int getInteger( RepositorySystemSession session, int defaultValue, String... keys )
@@ -161,9 +163,9 @@ public final class ConfigUtils
      * Gets the specified configuration property.
      * 
      * @param properties The configuration properties to read, must not be {@code null}.
-     * @param defaultValue The default value to return in case the property isn't set.
-     * @param keys The properties to read, must not be {@code null}. The specified keys are read one after one until a
-     *            valid value is found.
+     * @param defaultValue The default value to return in case none of the property keys is set to a number.
+     * @param keys The property keys to read, must not be {@code null}. The specified keys are read one after one until
+     *            a {@link Number} or a string representation of a {@link Long} is found.
      * @return The property value.
      */
     public static long getLong( Map<?, ?> properties, long defaultValue, String... keys )
@@ -195,9 +197,9 @@ public final class ConfigUtils
      * 
      * @param session The repository system session from which to read the configuration property, must not be
      *            {@code null}.
-     * @param defaultValue The default value to return in case the property isn't set.
-     * @param keys The properties to read, must not be {@code null}. The specified keys are read one after one until a
-     *            valid value is found.
+     * @param defaultValue The default value to return in case none of the property keys is set to a number.
+     * @param keys The property keys to read, must not be {@code null}. The specified keys are read one after one until
+     *            a {@link Number} or a string representation of a {@link Long} is found.
      * @return The property value.
      */
     public static long getLong( RepositorySystemSession session, long defaultValue, String... keys )
@@ -209,9 +211,9 @@ public final class ConfigUtils
      * Gets the specified configuration property.
      * 
      * @param properties The configuration properties to read, must not be {@code null}.
-     * @param defaultValue The default value to return in case the property isn't set.
-     * @param keys The properties to read, must not be {@code null}. The specified keys are read one after one until a
-     *            valid value is found.
+     * @param defaultValue The default value to return in case none of the property keys is set to a number.
+     * @param keys The property keys to read, must not be {@code null}. The specified keys are read one after one until
+     *            a {@link Number} or a string representation of a {@link Float} is found.
      * @return The property value.
      */
     public static float getFloat( Map<?, ?> properties, float defaultValue, String... keys )
@@ -243,9 +245,9 @@ public final class ConfigUtils
      * 
      * @param session The repository system session from which to read the configuration property, must not be
      *            {@code null}.
-     * @param defaultValue The default value to return in case the property isn't set.
-     * @param keys The properties to read, must not be {@code null}. The specified keys are read one after one until a
-     *            valid value is found.
+     * @param defaultValue The default value to return in case none of the property keys is set to a number.
+     * @param keys The property keys to read, must not be {@code null}. The specified keys are read one after one until
+     *            a {@link Number} or a string representation of a {@link Float} is found.
      * @return The property value.
      */
     public static float getFloat( RepositorySystemSession session, float defaultValue, String... keys )
@@ -257,9 +259,9 @@ public final class ConfigUtils
      * Gets the specified configuration property.
      * 
      * @param properties The configuration properties to read, must not be {@code null}.
-     * @param defaultValue The default value to return in case the property isn't set.
-     * @param keys The properties to read, must not be {@code null}. The specified keys are read one after one until a
-     *            valid value is found.
+     * @param defaultValue The default value to return in case none of the property keys is set to a boolean.
+     * @param keys The property keys to read, must not be {@code null}. The specified keys are read one after one until
+     *            a {@link Boolean} or a string (to be {@link Boolean#parseBoolean(String) parsed as boolean}) is found.
      * @return The property value.
      */
     public static boolean getBoolean( Map<?, ?> properties, boolean defaultValue, String... keys )
@@ -286,9 +288,9 @@ public final class ConfigUtils
      * 
      * @param session The repository system session from which to read the configuration property, must not be
      *            {@code null}.
-     * @param defaultValue The default value to return in case the property isn't set.
-     * @param keys The properties to read, must not be {@code null}. The specified keys are read one after one until a
-     *            valid value is found.
+     * @param defaultValue The default value to return in case none of the property keys is set to a boolean.
+     * @param keys The property keys to read, must not be {@code null}. The specified keys are read one after one until
+     *            a {@link Boolean} or a string (to be {@link Boolean#parseBoolean(String) parsed as boolean}) is found.
      * @return The property value.
      */
     public static boolean getBoolean( RepositorySystemSession session, boolean defaultValue, String... keys )
@@ -300,9 +302,9 @@ public final class ConfigUtils
      * Gets the specified configuration property.
      * 
      * @param properties The configuration properties to read, must not be {@code null}.
-     * @param defaultValue The default value to return in case the property isn't set, may be {@code null}.
-     * @param keys The properties to read, must not be {@code null}. The specified keys are read one after one until a
-     *            valid value is found.
+     * @param defaultValue The default value to return in case none of the property keys is set to a collection.
+     * @param keys The property keys to read, must not be {@code null}. The specified keys are read one after one until
+     *            a collection is found.
      * @return The property value or {@code null} if none.
      */
     public static List<?> getList( Map<?, ?> properties, List<?> defaultValue, String... keys )
@@ -329,9 +331,9 @@ public final class ConfigUtils
      * 
      * @param session The repository system session from which to read the configuration property, must not be
      *            {@code null}.
-     * @param defaultValue The default value to return in case the property isn't set, may be {@code null}.
-     * @param keys The properties to read, must not be {@code null}. The specified keys are read one after one until a
-     *            valid value is found.
+     * @param defaultValue The default value to return in case none of the property keys is set to a collection.
+     * @param keys The property keys to read, must not be {@code null}. The specified keys are read one after one until
+     *            a collection is found.
      * @return The property value or {@code null} if none.
      */
     public static List<?> getList( RepositorySystemSession session, List<?> defaultValue, String... keys )
@@ -343,9 +345,9 @@ public final class ConfigUtils
      * Gets the specified configuration property.
      * 
      * @param properties The configuration properties to read, must not be {@code null}.
-     * @param defaultValue The default value to return in case the property isn't set, may be {@code null}.
-     * @param keys The properties to read, must not be {@code null}. The specified keys are read one after one until a
-     *            valid value is found.
+     * @param defaultValue The default value to return in case none of the property keys is set to a map.
+     * @param keys The property keys to read, must not be {@code null}. The specified keys are read one after one until
+     *            a map is found.
      * @return The property value or {@code null} if none.
      */
     public static Map<?, ?> getMap( Map<?, ?> properties, Map<?, ?> defaultValue, String... keys )
@@ -368,9 +370,9 @@ public final class ConfigUtils
      * 
      * @param session The repository system session from which to read the configuration property, must not be
      *            {@code null}.
-     * @param defaultValue The default value to return in case the property isn't set, may be {@code null}.
-     * @param keys The properties to read, must not be {@code null}. The specified keys are read one after one until a
-     *            valid value is found.
+     * @param defaultValue The default value to return in case none of the property keys is set to a map.
+     * @param keys The property keys to read, must not be {@code null}. The specified keys are read one after one until
+     *            a map is found.
      * @return The property value or {@code null} if none.
      */
     public static Map<?, ?> getMap( RepositorySystemSession session, Map<?, ?> defaultValue, String... keys )
