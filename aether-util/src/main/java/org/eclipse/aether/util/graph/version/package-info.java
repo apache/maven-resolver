849d9e952ff4b768c920f200e9e922fbdf717464
Benjamin Bentmann <bentmann@sonatype.com>
2014-01-05 19:23:00 +0100

    Added package-level API docs

---
+++ aether-util/src/main/java/org/eclipse/aether/util/graph/version/package-info.java
@@ -0,0 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Sonatype, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Sonatype, Inc. - initial API and implementation
+ *******************************************************************************/
+/**
+ * Various version filters for building a dependency graph.
+ */
+package org.eclipse.aether.util.graph.version;
+
---
+++ aether-util/src/main/java/org/eclipse/aether/util/graph/visitor/package-info.java
@@ -0,0 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Sonatype, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Sonatype, Inc. - initial API and implementation
+ *******************************************************************************/
+/**
+ * Various dependency visitors for inspecting a dependency graph.
+ */
+package org.eclipse.aether.util.graph.visitor;
+
