e171c21e55afc366f56cd82130888c2b3e68248a
Benjamin Bentmann <bentmann@sonatype.com>
2014-11-01 19:30:29 +0100

    Bug 449599 - Return value of DefaultSessionData.set(key, null, null) disobeys API doc
    Fixed set(key, null, null) to return true if current mapping is null/non-existing

--- aether-api/src/main/java/org/eclipse/aether/DefaultSessionData.java
+++ aether-api/src/main/java/org/eclipse/aether/DefaultSessionData.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2012 Sonatype, Inc.
+ * Copyright (c) 2010, 2014 Sonatype, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -61,6 +61,10 @@ public final class DefaultSessionData
         }
         else
         {
+            if ( oldValue == null )
+            {
+                return !data.containsKey( key );
+            }
             return data.remove( key, oldValue );
         }
     }
