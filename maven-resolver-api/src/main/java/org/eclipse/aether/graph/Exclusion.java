bceb43a2880efe3fd9b59e78ad0fc8ca1ba68e80
Michael Osipov <michaelo@apache.org>
2020-08-06 21:15:31 +0200

    [MRESOLVER-129] Exclusion has no setters

--- maven-resolver-api/src/main/java/org/eclipse/aether/graph/Exclusion.java
+++ maven-resolver-api/src/main/java/org/eclipse/aether/graph/Exclusion.java
@@ -8,9 +8,9 @@ package org.eclipse.aether.graph;
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *  http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -20,9 +20,8 @@ package org.eclipse.aether.graph;
  */
 
 /**
- * An exclusion of one or more transitive dependencies. <em>Note:</em> Instances of this class are immutable and the
- * exposed mutators return new objects rather than changing the current instance.
- * 
+ * An exclusion of one or more transitive dependencies.
+ *
  * @see Dependency#getExclusions()
  */
 public final class Exclusion
@@ -38,7 +37,7 @@ public final class Exclusion
 
     /**
      * Creates an exclusion for artifacts with the specified coordinates.
-     * 
+     *
      * @param groupId The group identifier, may be {@code null}.
      * @param artifactId The artifact identifier, may be {@code null}.
      * @param classifier The classifier, may be {@code null}.
@@ -54,7 +53,7 @@ public final class Exclusion
 
     /**
      * Gets the group identifier for artifacts to exclude.
-     * 
+     *
      * @return The group identifier, never {@code null}.
      */
     public String getGroupId()
@@ -64,7 +63,7 @@ public final class Exclusion
 
     /**
      * Gets the artifact identifier for artifacts to exclude.
-     * 
+     *
      * @return The artifact identifier, never {@code null}.
      */
     public String getArtifactId()
@@ -74,7 +73,7 @@ public final class Exclusion
 
     /**
      * Gets the classifier for artifacts to exclude.
-     * 
+     *
      * @return The classifier, never {@code null}.
      */
     public String getClassifier()
@@ -84,7 +83,7 @@ public final class Exclusion
 
     /**
      * Gets the file extension for artifacts to exclude.
-     * 
+     *
      * @return The file extension of artifacts to exclude, never {@code null}.
      */
     public String getExtension()
