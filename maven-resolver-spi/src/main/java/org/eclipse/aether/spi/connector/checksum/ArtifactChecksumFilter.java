9720fdb9fed7e3e926f49e15f94d69d88a68b4be
Tamas Cservenak <tamas@cservenak.net>
2022-03-11 13:21:28 +0100

    Javadoc

--- maven-resolver-spi/src/main/java/org/eclipse/aether/spi/connector/checksum/ArtifactChecksumFilter.java
+++ maven-resolver-spi/src/main/java/org/eclipse/aether/spi/connector/checksum/ArtifactChecksumFilter.java
@@ -22,7 +22,7 @@ package org.eclipse.aether.spi.connector.checksum;
 import org.eclipse.aether.artifact.Artifact;
 
 /**
- * Filter that is able to tell does artifact should have expected checksums or not. Most notably, artifacts like
+ * Filter that is able to tell does artifact have expected checksums or not. Most notably, artifacts like
  * different kind of signatures will not have checksums.
  *
  * @since 1.8.0
@@ -30,9 +30,15 @@ import org.eclipse.aether.artifact.Artifact;
 public interface ArtifactChecksumFilter
 {
     /**
-     * Returns {@code true} if the given artifact does not have expected checksums. Hence, the artifact checksums will
-     * not be validated during transport. If {@code false} returned, the artifact will have its checksums fetched and
-     * validated.
+     * Tells whether given artifact have checksums according to current layout or not. If it returns {@code false},
+     * then layout configured checksums will be expected: on upload they will be calculated and deployed along
+     * artifact, on download they will be retrieved and validated.
+     *
+     * If it returns {@code true} the given artifacts will have checksums omitted: on upload they will not be
+     * calculated and deployed, and on download they will be not retrieved nor validated.
+     *
+     * Typical case to return {@code true} (to omit checksums) is for GPG signatures, that are already a "sub-artifact"
+     * of some main artifact (ie. a JAR), and they can be validated by some other means.
      */
     boolean omitChecksumsFor( Artifact artifact );
 }
