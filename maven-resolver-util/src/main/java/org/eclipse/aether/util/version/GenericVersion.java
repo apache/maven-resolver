a94e2d7bf04d0720f6489b4e45bab0b91ce1b8d8
Tamas Cservenak <tamas@cservenak.net>
2023-02-16 17:13:57 +0100

    Unused import

--- maven-resolver-util/src/main/java/org/eclipse/aether/util/version/GenericVersion.java
+++ maven-resolver-util/src/main/java/org/eclipse/aether/util/version/GenericVersion.java
@@ -21,7 +21,6 @@ package org.eclipse.aether.util.version;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
