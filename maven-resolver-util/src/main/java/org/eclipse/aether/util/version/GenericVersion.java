52bb9967b9f279e4b58aeaf05d9b09a5705ef3ba
Tamas Cservenak <tamas@cservenak.net>
2023-02-16 17:35:39 +0100

    Improve condition

--- maven-resolver-util/src/main/java/org/eclipse/aether/util/version/GenericVersion.java
+++ maven-resolver-util/src/main/java/org/eclipse/aether/util/version/GenericVersion.java
@@ -272,7 +272,7 @@ final class GenericVersion
                 }
                 countedChars++;
             }
-            return countedChars > 2 && countedChars % 2 == 0 && hasHexCandidates;
+            return hasHexCandidates && countedChars > 3 && countedChars % 2 == 0;
         }
 
         public boolean next()
