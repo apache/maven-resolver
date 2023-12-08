b7a6462789fc3cf2d9532780d88ef9279a69a5ee
Tamas Cservenak <tamas@cservenak.net>
2023-12-08 18:14:05 +0100

    This iface is safe as data
    But lacked warning to not implement it

--- maven-resolver-api/src/main/java/org/eclipse/aether/RepositoryCache.java
+++ maven-resolver-api/src/main/java/org/eclipse/aether/RepositoryCache.java
@@ -26,6 +26,8 @@ import java.util.function.Supplier;
  * Actual cache implementations must be thread-safe.
  *
  * @see RepositorySystemSession#getCache()
+ * @noimplement This interface is not intended to be implemented by clients.
+ * @noextend This interface is not intended to be extended by clients.
  */
 public interface RepositoryCache {
 
