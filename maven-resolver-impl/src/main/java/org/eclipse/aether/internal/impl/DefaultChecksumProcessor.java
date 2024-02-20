393d77c0631de11f0e6832511129030b5dd578be
Tamas Cservenak <tamas@cservenak.net>
2024-02-20 20:36:17 +0100

    [MRESOLVER-496] Paths everywhere, addendum 1 (#434)
    Addendum 1: fix buffered-reader creation
    
    ---
    
    https://issues.apache.org/jira/browse/MRESOLVER-496

--- maven-resolver-impl/src/main/java/org/eclipse/aether/internal/impl/DefaultChecksumProcessor.java
+++ maven-resolver-impl/src/main/java/org/eclipse/aether/internal/impl/DefaultChecksumProcessor.java
@@ -24,7 +24,6 @@ import javax.inject.Singleton;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -51,8 +50,7 @@ public class DefaultChecksumProcessor implements ChecksumProcessor {
     public String readChecksum(final Path checksumPath) throws IOException {
         // for now do exactly same as happened before, but FileProcessor is a component and can be replaced
         String checksum = "";
-        try (BufferedReader br = new BufferedReader(
-                new InputStreamReader(Files.newInputStream(checksumPath), StandardCharsets.UTF_8), 512)) {
+        try (BufferedReader br = Files.newBufferedReader(checksumPath, StandardCharsets.UTF_8)) {
             while (true) {
                 String line = br.readLine();
                 if (line == null) {
