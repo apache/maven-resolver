apache/maven-resolver

commit 08f102a4579a911afff8a8301cb1a708c3bc26af

[MRESOLVER-449] Step toward testing all HTTP transports (#391)
Right now, the HTTP test suite is pulled out and reused for all HTTP transports. It passes for ApacheTransporter (as it was pulled from it), but Jetty and JDK show some signs of some problems...

Current results (note: base class HttpTransporterTest has 70 tests, while some transport specific UTs add more):
* apache :heavy_check_mark: 74 test OK
* jdk :red_circle: 62 test OK, 9 test FAIL
* jetty :red_circle: 64 test OK, 6 FAIL

The "basics" are overall good (basics pass everywhere), failures stems from small differences.

Jetty: it seems it consumes the body (pulls bytes out of it) even before all the HTTP auth happens, this causes that `TransportListener` gets notified "transport started" but in fact is not and test asserts it to not. Also, Jetty throws in tests where server is set up to abruptly close the connection. Retries are not properly done (low level, like abrupt connection drop).

JDK: same problem with body (pulls bytes beforehand) and similarly as with Jetty, "transport started" events fails the test. Preemptive auth is not done (as it seems it is either "manual" handling of auth or Authenticator is used but then preemptive auth becomes impossible as headers are removed, no way to set them). It also throws in tests where server abruptly drops connection. Same for retries (in case of TCP issue).

---

https://issues.apache.org/jira/browse/MRESOLVER-449

maven-resolver-transport-jdk-parent/maven-resolver-transport-jdk-11/src/main/java/org/eclipse/aether/transport/jdk/JdkTransporter.java
@@ -18,29 +18,21 @@
  */
 package org.eclipse.aether.transport.jdk;
 
-import javax.net.ssl.SSLContext;
+import javax.net.ssl.*;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.net.Authenticator;
-import java.net.ConnectException;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.PasswordAuthentication;
-import java.net.ProxySelector;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.UnknownHostException;
+import java.net.*;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.nio.file.attribute.FileTime;
-import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.ZoneId;
@@ -64,6 +56,8 @@ import org.eclipse.aether.spi.connector.transport.GetTask;
 import org.eclipse.aether.spi.connector.transport.PeekTask;
 import org.eclipse.aether.spi.connector.transport.PutTask;
 import org.eclipse.aether.spi.connector.transport.TransportTask;
+import org.eclipse.aether.spi.connector.transport.http.HttpTransporter;
+import org.eclipse.aether.spi.connector.transport.http.HttpTransporterException;
 import org.eclipse.aether.transfer.NoTransporterException;
 import org.eclipse.aether.util.ConfigUtils;
 import org.eclipse.aether.util.FileUtils;
@@ -79,7 +73,7 @@ import static org.eclipse.aether.transport.jdk.JdkTransporterConfigurationKeys.D
  * @since 2.0.0
  */
 @SuppressWarnings({"checkstyle:magicnumber"})
-final class JdkTransporter extends AbstractTransporter {
+final class JdkTransporter extends AbstractTransporter implements HttpTransporter {
     private static final Logger LOGGER = LoggerFactory.getLogger(JdkTransporter.class);
 
     private static final DateTimeFormatter RFC7231 = DateTimeFormatter.ofPattern(
@@ -202,7 +196,8 @@ final class JdkTransporter extends AbstractTransporter {
 
     @Override
     public int classify(Throwable error) {
-        if (error instanceof JdkException && ((JdkException) error).getStatusCode() == NOT_FOUND) {
+        if (error instanceof HttpTransporterException
+                && ((HttpTransporterException) error).getStatusCode() == NOT_FOUND) {
             return ERROR_NOT_FOUND;
         }
         return ERROR_OTHER;
@@ -218,7 +213,7 @@ final class JdkTransporter extends AbstractTransporter {
         try {
             HttpResponse<Void> response = client.send(request.build(), HttpResponse.BodyHandlers.discarding());
             if (response.statusCode() >= MULTIPLE_CHOICES) {
-                throw new JdkException(response.statusCode());
+                throw new HttpTransporterException(response.statusCode());
             }
         } catch (ConnectException e) {
             throw enhance(e);
@@ -254,7 +249,7 @@ final class JdkTransporter extends AbstractTransporter {
                         resume = false;
                         continue;
                     }
-                    throw new JdkException(response.statusCode());
+                    throw new HttpTransporterException(response.statusCode());
                 }
             } catch (ConnectException e) {
                 throw enhance(e);
@@ -343,7 +338,7 @@ final class JdkTransporter extends AbstractTransporter {
             try {
                 HttpResponse<Void> response = client.send(request.build(), HttpResponse.BodyHandlers.discarding());
                 if (response.statusCode() >= MULTIPLE_CHOICES) {
-                    throw new JdkException(response.statusCode());
+                    throw new HttpTransporterException(response.statusCode());
                 }
             } catch (ConnectException e) {
                 throw enhance(e);
@@ -425,6 +420,18 @@ final class JdkTransporter extends AbstractTransporter {
             throws NoTransporterException {
         final String instanceKey = HTTP_INSTANCE_KEY_PREFIX + repository.getId();
 
+        final String httpsSecurityMode = ConfigUtils.getString(
+                session,
+                ConfigurationProperties.HTTPS_SECURITY_MODE_DEFAULT,
+                ConfigurationProperties.HTTPS_SECURITY_MODE + "." + repository.getId(),
+                ConfigurationProperties.HTTPS_SECURITY_MODE);
+
+        if (!ConfigurationProperties.HTTPS_SECURITY_MODE_DEFAULT.equals(httpsSecurityMode)
+                && !ConfigurationProperties.HTTPS_SECURITY_MODE_INSECURE.equals(httpsSecurityMode)) {
+            throw new IllegalArgumentException("Unsupported '" + httpsSecurityMode + "' HTTPS security mode.");
+        }
+        final boolean insecure = ConfigurationProperties.HTTPS_SECURITY_MODE_INSECURE.equals(httpsSecurityMode);
+
         // todo: normally a single client per JVM is sufficient - in particular cause part of the config
         //       is global and not per instance so we should create a client only when conf changes for a repo
         //       else fallback on a global client
@@ -448,7 +455,40 @@ final class JdkTransporter extends AbstractTransporter {
                     }
 
                     if (sslContext == null) {
-                        sslContext = SSLContext.getDefault();
+                        if (insecure) {
+                            sslContext = SSLContext.getInstance("TLS");
+                            X509ExtendedTrustManager tm = new X509ExtendedTrustManager() {
+                                @Override
+                                public void checkClientTrusted(X509Certificate[] chain, String authType) {}
+
+                                @Override
+                                public void checkServerTrusted(X509Certificate[] chain, String authType) {}
+
+                                @Override
+                                public void checkClientTrusted(
+                                        X509Certificate[] chain, String authType, Socket socket) {}
+
+                                @Override
+                                public void checkServerTrusted(
+                                        X509Certificate[] chain, String authType, Socket socket) {}
+
+                                @Override
+                                public void checkClientTrusted(
+                                        X509Certificate[] chain, String authType, SSLEngine engine) {}
+
+                                @Override
+                                public void checkServerTrusted(
+                                        X509Certificate[] chain, String authType, SSLEngine engine) {}
+
+                                @Override
+                                public X509Certificate[] getAcceptedIssuers() {
+                                    return null;
+                                }
+                            };
+                            sslContext.init(null, new X509TrustManager[] {tm}, null);
+                        } else {
+                            sslContext = SSLContext.getDefault();
+                        }
                     }
 
                     int connectTimeout = ConfigUtils.getInteger(
@@ -466,6 +506,12 @@ final class JdkTransporter extends AbstractTransporter {
                             .followRedirects(HttpClient.Redirect.NORMAL)
                             .connectTimeout(Duration.ofMillis(connectTimeout))
                             .sslContext(sslContext);
+
+                    if (insecure) {
+                        SSLParameters sslParameters = new SSLParameters();
+                        sslParameters.setEndpointIdentificationAlgorithm(null);
+                        builder.sslParameters(sslParameters);
+                    }
 
                     setLocalAddress(builder, () -> getHttpLocalAddress(session, repository));
 
@@ -512,7 +558,7 @@ final class JdkTransporter extends AbstractTransporter {
                     }
 
                     return result;
-                } catch (NoSuchAlgorithmException e) {
+                } catch (Exception e) {
                     throw new WrapperEx(e);
                 }
             });

maven-resolver-spi/src/main/java/org/eclipse/aether/spi/connector/transport/http/HttpTransporter.java
@@ -16,22 +16,13 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.eclipse.aether.transport.jdk;
+package org.eclipse.aether.spi.connector.transport.http;
+
+import org.eclipse.aether.spi.connector.transport.Transporter;
 
 /**
- * Exception thrown by {@link JdkTransporter} in case of errors.
+ * A transporter using HTTP protocol.
  *
  * @since 2.0.0
  */
-final class JdkException extends Exception {
-    private final int statusCode;
-
-    JdkException(int statusCode) {
-        super("HTTP Status: " + statusCode);
-        this.statusCode = statusCode;
-    }
-
-    public int getStatusCode() {
-        return statusCode;
-    }
-}
+public interface HttpTransporter extends Transporter {}

maven-resolver-spi/src/main/java/org/eclipse/aether/spi/connector/transport/http/HttpTransporterException.java
@@ -16,17 +16,17 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.eclipse.aether.transport.jetty;
+package org.eclipse.aether.spi.connector.transport.http;
 
 /**
- * Exception thrown by {@link JettyTransporter} in case of errors.
+ * Exception thrown by {@link HttpTransporter} in case of errors.
  *
  * @since 2.0.0
  */
-final class JettyException extends Exception {
+public class HttpTransporterException extends Exception {
     private final int statusCode;
 
-    JettyException(int statusCode) {
+    public HttpTransporterException(int statusCode) {
         super("HTTP Status: " + statusCode);
         this.statusCode = statusCode;
     }

maven-resolver-spi/src/main/java/org/eclipse/aether/spi/connector/transport/http/HttpTransporterFactory.java
@@ -1,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.eclipse.aether.spi.connector.transport.http;
+
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.repository.RemoteRepository;
+import org.eclipse.aether.spi.connector.transport.TransporterFactory;
+import org.eclipse.aether.transfer.NoTransporterException;
+
+/**
+ * A factory for {@link HttpTransporter}.
+ *
+ * @since 2.0.0
+ */
+public interface HttpTransporterFactory extends TransporterFactory {
+
+    /**
+     * Tries to create HTTP transporter for the specified remote repository.
+     *
+     * @param session The repository system session from which to configure the transporter, must not be {@code null}.
+     *            In particular, a transporter should obey the timeouts configured for the session.
+     * @param repository The remote repository to create a transporter for, must not be {@code null}.
+     * @return The transporter for the given repository, never {@code null}.
+     * @throws NoTransporterException If the factory cannot create a transporter for the specified remote repository.
+     */
+    @Override
+    HttpTransporter newInstance(RepositorySystemSession session, RemoteRepository repository)
+            throws NoTransporterException;
+}

maven-resolver-test-http/pom.xml
@@ -69,6 +69,18 @@
       <artifactId>jetty-http</artifactId>
     </dependency>
     <dependency>
+      <groupId>org.eclipse.jetty</groupId>
+      <artifactId>jetty-alpn-server</artifactId>
+    </dependency>
+    <dependency>
+      <groupId>org.eclipse.jetty</groupId>
+      <artifactId>jetty-alpn-java-server</artifactId>
+    </dependency>
+    <dependency>
+      <groupId>org.eclipse.jetty.http2</groupId>
+      <artifactId>http2-server</artifactId>
+    </dependency>
+    <dependency>
       <groupId>org.slf4j</groupId>
       <artifactId>slf4j-api</artifactId>
     </dependency>

maven-resolver-test-http/src/main/java/org/eclipse/aether/internal/test/util/http/HttpServer.java
@@ -39,15 +39,11 @@ import java.util.regex.Pattern;
 
 import org.eclipse.aether.internal.impl.checksum.Sha1ChecksumAlgorithmFactory;
 import org.eclipse.aether.spi.connector.checksum.ChecksumAlgorithmHelper;
+import org.eclipse.jetty.alpn.server.ALPNServerConnectionFactory;
 import org.eclipse.jetty.http.HttpHeader;
 import org.eclipse.jetty.http.HttpMethod;
-import org.eclipse.jetty.server.HttpConfiguration;
-import org.eclipse.jetty.server.HttpConnectionFactory;
-import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.Response;
-import org.eclipse.jetty.server.SecureRequestCustomizer;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.http2.server.HTTP2ServerConnectionFactory;
+import org.eclipse.jetty.server.*;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.util.IO;
@@ -162,24 +158,37 @@ public class HttpServer {
     private HttpServer addSslConnector(boolean needClientAuth) {
         if (httpsConnector == null) {
             SslContextFactory.Server ssl = new SslContextFactory.Server();
-            if (needClientAuth) {
-                ssl.setNeedClientAuth(true);
-                ssl.setKeyStorePath(new File("src/test/resources/ssl/server-store").getAbsolutePath());
+            ssl.setNeedClientAuth(needClientAuth);
+            if (!needClientAuth) {
+                ssl.setKeyStorePath(HttpTransporterTest.KEY_STORE_SELF_SIGNED_PATH
+                        .toAbsolutePath()
+                        .toString());
                 ssl.setKeyStorePassword("server-pwd");
-                ssl.setTrustStorePath(new File("src/test/resources/ssl/client-store").getAbsolutePath());
-                ssl.setTrustStorePassword("client-pwd");
                 ssl.setSniRequired(false);
             } else {
-                ssl.setNeedClientAuth(false);
-                ssl.setKeyStorePath(new File("src/test/resources/ssl/server-store-selfsigned").getAbsolutePath());
+                ssl.setKeyStorePath(
+                        HttpTransporterTest.KEY_STORE_PATH.toAbsolutePath().toString());
                 ssl.setKeyStorePassword("server-pwd");
+                ssl.setTrustStorePath(
+                        HttpTransporterTest.TRUST_STORE_PATH.toAbsolutePath().toString());
+                ssl.setTrustStorePassword("client-pwd");
                 ssl.setSniRequired(false);
             }
+
             HttpConfiguration httpsConfig = new HttpConfiguration();
             SecureRequestCustomizer customizer = new SecureRequestCustomizer();
             customizer.setSniHostCheck(false);
             httpsConfig.addCustomizer(customizer);
-            httpsConnector = new ServerConnector(server, ssl, new HttpConnectionFactory(httpsConfig));
+
+            HttpConnectionFactory http1 = new HttpConnectionFactory(httpsConfig);
+
+            HTTP2ServerConnectionFactory http2 = new HTTP2ServerConnectionFactory(httpsConfig);
+
+            ALPNServerConnectionFactory alpn = new ALPNServerConnectionFactory();
+            alpn.setDefaultProtocol(http1.getProtocol());
+
+            SslConnectionFactory tls = new SslConnectionFactory(ssl, alpn.getProtocol());
+            httpsConnector = new ServerConnector(server, tls, alpn, http2, http1);
             server.addConnector(httpsConnector);
             try {
                 httpsConnector.start();
@@ -268,6 +277,7 @@ public class HttpServer {
     }
 
     private class ConnectionClosingHandler extends AbstractHandler {
+        @Override
         public void handle(String target, Request req, HttpServletRequest request, HttpServletResponse response) {
             if (connectionsToClose.getAndDecrement() > 0) {
                 Response jettyResponse = (Response) response;
@@ -277,7 +287,7 @@ public class HttpServer {
     }
 
     private class LogHandler extends AbstractHandler {
-
+        @Override
         public void handle(String target, Request req, HttpServletRequest request, HttpServletResponse response) {
             LOGGER.info(
                     "{} {}{}",
@@ -304,7 +314,7 @@ public class HttpServer {
     private static final Pattern SIMPLE_RANGE = Pattern.compile("bytes=([0-9])+-");
 
     private class RepoHandler extends AbstractHandler {
-
+        @Override
         public void handle(String target, Request req, HttpServletRequest request, HttpServletResponse response)
                 throws IOException {
             String path = req.getPathInfo().substring(1);
@@ -438,7 +448,7 @@ public class HttpServer {
     }
 
     private class RedirectHandler extends AbstractHandler {
-
+        @Override
         public void handle(String target, Request req, HttpServletRequest request, HttpServletResponse response) {
             String path = req.getPathInfo();
             if (!path.startsWith("/redirect/")) {
@@ -465,7 +475,7 @@ public class HttpServer {
     }
 
     private class AuthHandler extends AbstractHandler {
-
+        @Override
         public void handle(String target, Request req, HttpServletRequest request, HttpServletResponse response)
                 throws IOException {
             if (ExpectContinue.BROKEN.equals(expectContinue)
@@ -485,7 +495,7 @@ public class HttpServer {
     }
 
     private class ProxyAuthHandler extends AbstractHandler {
-
+        @Override
         public void handle(String target, Request req, HttpServletRequest request, HttpServletResponse response) {
             if (proxyUsername != null && proxyPassword != null) {
                 if (checkBasicAuth(

maven-resolver-test-http/src/main/java/org/eclipse/aether/internal/test/util/http/RecordingTransportListener.java
@@ -56,7 +56,14 @@ public class RecordingTransportListener extends TransportListener {
     @Override
     public void transportProgressed(ByteBuffer data) throws TransferCancelledException {
         progressedCount++;
-        baos.write(data.array(), data.arrayOffset() + ((Buffer) data).position(), data.remaining());
+        if (data.hasArray()) {
+            baos.write(data.array(), data.arrayOffset() + ((Buffer) data).position(), data.remaining());
+        } else {
+            byte[] arr = new byte[data.remaining()];
+            data.mark();
+            data.get(arr);
+            data.reset();
+        }
         if (cancelProgress) {
             throw new TransferCancelledException();
         }

commit 3927899b2c76fa7850559490cd6458e1ac0f601d

[MRESOLVER-579] Fix overwrite of SSLParameters in JDK HTTP transport when securityMode is "insecure" (#529)
I have been experimenting with Maven 4.0.0-beta-4, specifically testing its HTTP/2 support. During my tests, I used a self-signed certificate for the testing repository and disabled TLS validation. This approach produced unexpected behavior. While TLS certificate validation was indeed disabled as expected, it also caused the ALPN extension to be omitted from the Client Hello message.

To further investigate, I added the self-signed certificate to the JDK's cacerts keystore and removed the insecure option. With this configuration, ALPN support was restored, and HTTP/2 worked correctly again. This behavior can lead to problems if the server prioritizes HTTP/2 or does not support HTTP/1.1.

This change addresses an issue where SSLParameters were being overwritten (introduced in 08f102a4579a911afff8a8301cb1a708c3bc26af), causing the loss of multiple TLS extensions, including ALPN and SNI. Setting the `aether.transport.https.securityMode=insecure` property disables TLS validation but also inadvertently disabled ALPN and SNI.

Now, SSLParameters are derived from SSLContext defaults to ensure proper handling of these extensions, even when TLS validation is disabled in JDK HTTP transport.

---

https://issues.apache.org/jira/browse/MRESOLVER-579

maven-resolver-transport-jdk-parent/maven-resolver-transport-jdk-11/src/main/java/org/eclipse/aether/transport/jdk/JdkTransporter.java
@@ -488,7 +488,7 @@ final class JdkTransporter extends AbstractTransporter implements HttpTransporte
                             .sslContext(sslContext);
 
                     if (insecure) {
-                        SSLParameters sslParameters = new SSLParameters();
+                        SSLParameters sslParameters = sslContext.getDefaultSSLParameters();
                         sslParameters.setEndpointIdentificationAlgorithm(null);
                         builder.sslParameters(sslParameters);
                     }