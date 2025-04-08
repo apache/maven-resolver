e327728d54ce85662af95ea1afea762c94fcb1f6
Tamas Cservenak <tamas@cservenak.net>
2025-04-08 14:36:08 +0200

    [MRESOLVER-685] Connector pipelining (#679)
    Ability to "pipe" connectors one onto another in controlled and configured fashion.
    
    The ide is following:
    - first (actually connecting to remote) connector is chosen as today (based on priority)
    - next, new type of factories PipelinedRepositoryConnectorFactory is ordered (by priority) and they can (but does not have to) wrap the delegate in configured order.
    
    Factored out existing RRF connector into new factory, and introduced another OfflinePRCF, as so far, "resolver offline" was in fact managed at different spots, and user was still able to circumvent offline setting. This now connector now wraps connector and refuses going remote if session if offline.
    
    Vanilla resolver does this:
    `offline( rrf( basic( repo ) ) )`
    
    Ultimate goal, envision this:
    `offline( mimir( sigcheck( rrf( basic( repo ) ) ) ) )`
    
    ---
    
    https://issues.apache.org/jira/browse/MRESOLVER-685

---
+++ maven-resolver-impl/src/main/java/org/eclipse/aether/internal/impl/filter/FilteringPipelineRepositoryConnectorFactory.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.eclipse.aether.internal.impl.filter;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.impl.RemoteRepositoryFilterManager;
+import org.eclipse.aether.repository.RemoteRepository;
+import org.eclipse.aether.spi.connector.PipelineRepositoryConnectorFactory;
+import org.eclipse.aether.spi.connector.RepositoryConnector;
+import org.eclipse.aether.spi.connector.filter.RemoteRepositoryFilter;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * A filtering connector factory.
+ *
+ * @since TBD
+ */
+@Singleton
+@Named(FilteringPipelineRepositoryConnectorFactory.NAME)
+public final class FilteringPipelineRepositoryConnectorFactory implements PipelineRepositoryConnectorFactory {
+    public static final String NAME = "rrf";
+
+    private final RemoteRepositoryFilterManager remoteRepositoryFilterManager;
+
+    /**
+     * This connector should be usually the right-most in pipeline.
+     */
+    private float priority = 10000;
+
+    @Inject
+    public FilteringPipelineRepositoryConnectorFactory(RemoteRepositoryFilterManager remoteRepositoryFilterManager) {
+        this.remoteRepositoryFilterManager = requireNonNull(remoteRepositoryFilterManager);
+    }
+
+    @Override
+    public RepositoryConnector newInstance(
+            RepositorySystemSession session, RemoteRepository repository, RepositoryConnector delegate) {
+        RemoteRepositoryFilter filter = remoteRepositoryFilterManager.getRemoteRepositoryFilter(session);
+        if (filter != null) {
+            return new FilteringRepositoryConnector(repository, delegate, filter);
+        } else {
+            return delegate;
+        }
+    }
+
+    @Override
+    public float getPriority() {
+        return priority;
+    }
+
+    public FilteringPipelineRepositoryConnectorFactory setPriority(float priority) {
+        this.priority = priority;
+        return this;
+    }
+}
---
+++ maven-resolver-spi/src/main/java/org/eclipse/aether/spi/connector/PipelineRepositoryConnectorFactory.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.eclipse.aether.spi.connector;
+
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.repository.RemoteRepository;
+
+/**
+ * A pipeline factory to create piped repository connectors.
+ *
+ * @since TBD
+ */
+public interface PipelineRepositoryConnectorFactory {
+
+    /**
+     * Create a piped repository connector for the specified remote repository. Typically, a factory will inspect
+     * {@link RemoteRepository#getProtocol()} and {@link RemoteRepository#getContentType()} to determine whether it can
+     * handle a repository. This method never throws or returns {@code null}, least can do is to return the passed in
+     * delegate connector instance.
+     *
+     * @param session The repository system session from which to configure the connector, must not be {@code null}. In
+     *            particular, a connector must notify any {@link RepositorySystemSession#getTransferListener()} set for
+     *            the session and should obey the timeouts configured for the session.
+     * @param repository The remote repository to create a connector for, must not be {@code null}.
+     * @param delegate The delegate connector, never {@code null}. The delegate is "right hand" connector in connector
+     *                 pipeline.
+     * @return The connector for the given repository, never {@code null}. If pipeline wants to step aside, it must
+     * return the passed in delegate connector instance.
+     */
+    RepositoryConnector newInstance(
+            RepositorySystemSession session, RemoteRepository repository, RepositoryConnector delegate);
+
+    /**
+     * The priority of this pipeline factory. Higher priority makes connector closer to right end (tail) of pipeline
+     * (closest to delegate), while lower priority makes it closer to left hand (head) of the pipeline.
+     */
+    float getPriority();
+}
