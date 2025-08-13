7b4bd230b43d20cad022ddd0cfc6df0c362fe28d
Tamas Cservenak <tamas@cservenak.net>
2025-08-13 19:19:17 +0200

    Resolver Thread management (#1541)
    Rework thread management.
    
    High level changes:
    * introduce simple `SmartExecutor` to hide underlying things; offer minimal surface just enough for consumers
    * make 3 consumers use them with try-with-resources consistently (basic connector, metadata resolver, BF collector)
    * on Java 21 use virtual threads, as all 3 consumer executes very IO bound tasks (ideal contenders)
    
    Details:
    * consumer basic connector logic simplified; instead of going directly for "direct" executor, use one single method, and perform direct executions as needed (as it had already all in place already); basic connector retains the ability to "direct execute" tasks when appropriate (as it knows ahead the task count)
    * all 3 consumers use session cached `SmartExecutor` instances (basic connector exceptions for direct execution explained above)
    
    Consequences: the parameters
    * for basic connector: `aether.connector.basic.downstreamThreads`, `aether.connector.basic.upstreamThreads` and `aether.connector.basic.threads` (they may be suffixed with `.repoId`)
    * for BF collector `aether.dependencyCollector.bf.threads`
    * for metadata resolver `aether.metadataResolver.threads`
    
    are from now on **global and reflect the reality for maven session**, in a way they are now truly aplied per-Maven session (limits now represent true per-session limits).
    
    Before, this was not the case, and Maven was able to fully "step over" these limits: consider BF collector, it created always "local" thread pool with size 5. But alas, Maven 4 builds project models already in parallel (!), hence, BF collector (to resolve models for model builder) was already called from several threads, and each thread created "local" pool for it's own use.
    
    This _may imply_ that pool sizes _may need a revisit_, as they _may_ be too conservative now (or they were just not truly applied, as intended).
    
    
    Fixes #1537

---
+++ maven-resolver-util/src/main/java/org/eclipse/aether/util/concurrency/SmartExecutor.java
@@ -0,0 +1,164 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.eclipse.aether.util.concurrency;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Semaphore;
+
+/**
+ * Utilities for executors and sizing them.
+ * <em>Big fat note:</em> Do not use this class outside of resolver. This and related classes are not meant as "drop
+ * in replacement" for Jave Executors, is used in very controlled fashion only.
+ *
+ * @since 2.0.11
+ */
+public interface SmartExecutor extends AutoCloseable {
+    /**
+     * Submits a {@link Runnable} to execution.
+     */
+    default void submit(Runnable runnable) {
+        submit(() -> {
+            runnable.run();
+            return null;
+        });
+    }
+
+    /**
+     * Submits a {@link Callable} to execution, returns a {@link CompletableFuture}.
+     */
+    <T> CompletableFuture<T> submit(Callable<T> callable);
+
+    /**
+     * Shut down this instance (ideally used in try-with-resource construct).
+     */
+    void close();
+
+    /**
+     * Direct executor (caller executes).
+     */
+    class Direct implements SmartExecutor {
+        @Override
+        public <T> CompletableFuture<T> submit(Callable<T> callable) {
+            CompletableFuture<T> future = new CompletableFuture<>();
+            try {
+                future.complete(callable.call());
+            } catch (Exception e) {
+                future.completeExceptionally(e);
+            }
+            return future;
+        }
+
+        @Override
+        public void close() {}
+    }
+
+    /**
+     * Pooled executor backed by {@link ExecutorService}.
+     */
+    class Pooled implements SmartExecutor {
+        private final ExecutorService executor;
+
+        Pooled(ExecutorService executor) {
+            this.executor = executor;
+        }
+
+        @Override
+        public <T> CompletableFuture<T> submit(Callable<T> callable) {
+            CompletableFuture<T> future = new CompletableFuture<>();
+            executor.submit(() -> {
+                try {
+                    future.complete(callable.call());
+                } catch (Exception e) {
+                    future.completeExceptionally(e);
+                }
+            });
+            return future;
+        }
+
+        @Override
+        public void close() {
+            executor.shutdown();
+        }
+    }
+
+    /**
+     * Limited executor, where the actual goal is to protect accessed resource, like when virtual threads
+     * are being used, so the "pool" itself does not provide any kind of back-pressure.
+     */
+    class Limited implements SmartExecutor {
+        private final ExecutorService executor;
+        private final Semaphore semaphore;
+
+        Limited(ExecutorService executor, int limit) {
+            this.executor = executor;
+            this.semaphore = new Semaphore(limit);
+        }
+
+        @Override
+        public <T> CompletableFuture<T> submit(Callable<T> callable) {
+            try {
+                semaphore.acquire();
+                CompletableFuture<T> future = new CompletableFuture<>();
+                executor.submit(() -> {
+                    try {
+                        future.complete(callable.call());
+                    } catch (Exception e) {
+                        future.completeExceptionally(e);
+                    } finally {
+                        semaphore.release();
+                    }
+                });
+                return future;
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                CompletableFuture<T> failed = new CompletableFuture<>();
+                failed.completeExceptionally(e);
+                return failed;
+            }
+        }
+
+        @Override
+        public void close() {
+            executor.shutdown();
+        }
+    }
+
+    /**
+     * Wrapper to prevent closing.
+     */
+    class NonClosing implements SmartExecutor {
+        private final SmartExecutor smartExecutor;
+
+        NonClosing(SmartExecutor smartExecutor) {
+            this.smartExecutor = smartExecutor;
+        }
+
+        @Override
+        public <T> CompletableFuture<T> submit(Callable<T> callable) {
+            return smartExecutor.submit(callable);
+        }
+
+        @Override
+        public void close() {
+            // nope; delegate is managed
+        }
+    }
+}
---
+++ maven-resolver-util/src/main/java/org/eclipse/aether/util/concurrency/SmartExecutorUtils.java
@@ -0,0 +1,104 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.eclipse.aether.util.concurrency;
+
+import java.util.concurrent.Executors;
+
+import org.eclipse.aether.RepositorySystemSession;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Utilities for executors and sizing them.
+ * <em>Big fat note:</em> Do not use this class outside of resolver. This and related classes are not meant as "drop
+ * in replacement" for Jave Executors, is used in very controlled fashion only.
+ *
+ * @since 2.0.11
+ */
+public final class SmartExecutorUtils {
+    private static final SmartExecutor DIRECT = new SmartExecutor.Direct();
+
+    private SmartExecutorUtils() {}
+
+    /**
+     * Returns a smart executor for given parameters. If {@code tasks} is known (non-null), it must be greater than 0.
+     * The {@code maxConcurrentTasks} also must be greater than 0. The {@code namePrefix} must be non-null.
+     * <p>
+     * If {@code tasks} is set (is known), and equals to 1 (one), or {@code maxConcurrentTasks} equals to 1, the
+     * {@link #DIRECT} executor is returned, otherwise pooled one.
+     * <p>
+     * If @code tasks} is not set (is null), pooled one is returned with pool size of {@code maxConcurrentTasks}. In
+     * this case caller is advised to <em>reuse created executor across session</em>.
+     * <p>
+     * The returned instance must be closed out, ideally in try-with-resources construct. Returned instances when
+     * {@code tasks} parameter is given should not be cached (like in a session) as they may return {@link #DIRECT}
+     * executor for one call and a pool for subsequent call, based on value of tasks.
+     *
+     * @param tasks The amount of tasks, if known, {@code null} otherwise.
+     * @param maxConcurrentTasks The maximum concurrency caller wants.
+     * @param namePrefix The thread name prefixes, must not be {@code null).}
+     */
+    public static SmartExecutor newSmartExecutor(Integer tasks, int maxConcurrentTasks, String namePrefix) {
+        if (maxConcurrentTasks < 1) {
+            throw new IllegalArgumentException("maxConcurrentTasks must be > 0");
+        }
+        requireNonNull(namePrefix);
+        int poolSize;
+        if (tasks != null) {
+            if (tasks < 1) {
+                throw new IllegalArgumentException("tasks must be > 0");
+            }
+            if (tasks == 1 || maxConcurrentTasks == 1) {
+                return DIRECT;
+            }
+            poolSize = Math.min(tasks, maxConcurrentTasks);
+        } else {
+            if (maxConcurrentTasks == 1) {
+                return DIRECT;
+            }
+            poolSize = maxConcurrentTasks;
+        }
+        return new SmartExecutor.Pooled(Executors.newFixedThreadPool(poolSize, new WorkerThreadFactory(namePrefix)));
+    }
+
+    /**
+     * Returns a smart executor, bound to session if tasks to execute are not known ahead of time. The returned
+     * instance should be handled transparently, so preferably in try-with-resource even if underlying executor is
+     * probably tied to session lifecycle, if applicable.
+     * <p>
+     * Implementation note: by this change, the caller "concurrency" is made deterministic and global(!). If you consider
+     * collector example, it is called from project builder that in Maven 4 is already multithreaded, and before this
+     * change the actual threads doing IO (HTTP) was {@code callerThreadCount x maxConcurrentTask} per JVM/Maven process.
+     * Now, the {@code maxConcurrentTask} becomes global limit, and hence can be upped without unexpected "explosion"
+     * in increasing build threading or anything.
+     */
+    public static SmartExecutor smartExecutor(
+            RepositorySystemSession session, Integer tasks, int maxConcurrentTasks, String namePrefix) {
+        if (tasks == null) {
+            return (SmartExecutor)
+                    session.getData().computeIfAbsent(SmartExecutor.class.getSimpleName() + "-" + namePrefix, () -> {
+                        SmartExecutor smartExecutor = newSmartExecutor(tasks, maxConcurrentTasks, namePrefix);
+                        session.addOnSessionEndedHandler(smartExecutor::close);
+                        return new SmartExecutor.NonClosing(smartExecutor);
+                    });
+        } else {
+            return newSmartExecutor(tasks, maxConcurrentTasks, namePrefix);
+        }
+    }
+}
