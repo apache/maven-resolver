ef2b075db337b95579be14c3419a75895af560b8
Tamas Cservenak <tamas@cservenak.net>
2026-06-08 19:04:44 +0200

    Bugfix: Fix NPE in PathConflictResolver (#1904)
    Fixes #1903 bug in `PathConflictResolver` (PCR).
    
    Also, the fix in 5e97fd15f8dbc562c481252bc8ea1037992e6407 is wrong, and this PR redoes now all.
    
    Problem was cycle detection. Original (2.0.18) PCR did left "uncovered" nodes, still causing cycles, that should have been removed by PCR (causing SO in client code). The commit above remedied it, but caused another issue how cycles were detected (it opted to reuse existing `CYCLIC_CONFLICT_IDS`), but was again wrong.
    
    Then I realized: PCR by its design is "rebuilding" tree with `conflictIds` on Path instances, so it can easily spot properly when a cycle is about to happen (when a next node is partitioned into same `CONFLICT_ID` that is already contained in the path to root.
    
    Proof: the demo code (added comment) is now working as expected, plus, Quarkus build is buildable with Maven 3.10-SNAPSHOT using Resolver with this patch.
    
    As an interesting side-effect, this PR also fixes an issue applicable to _all released maven versions so far_:
    https://gist.github.com/cstamas/e24577162083f1e100d78a3e5d398cd3
    
    As the demo now produces "proper" tree as:
    ```
    dom4j:dom4j:jar:1.6.1 [compile]
    +- jaxme:jaxme-api:jar:0.3 [compile, optional]
    +- jaxen:jaxen:jar:1.1-beta-6 [compile, optional]
    |  +- jdom:jdom:jar:1.0 [compile, optional]
    |  +- xerces:xmlParserAPIs:jar:2.6.2 [compile, optional]
    |  +- xerces:xercesImpl:jar:2.6.2 [compile, optional]
    |  \- xom:xom:jar:1.0b3 [compile, optional]
    |     +- com.ibm.icu:icu4j:jar:2.6.1 [compile, optional]
    |     +- xalan:xalan:jar:2.6.0 [compile, optional]
    |     \- org.ccil.cowan.tagsoup:tagsoup:jar:0.9.7 [compile, optional]
    +- msv:xsdlib:jar:20030807 [compile, optional]
    +- msv:relaxngDatatype:jar:20030807 [compile, optional]
    +- pull-parser:pull-parser:jar:2 [compile, optional]
    +- xpp3:xpp3:jar:1.1.3.3 [compile, optional]
    +- stax:stax-api:jar:1.0 [compile, optional]
    \- xml-apis:xml-apis:jar:1.0.b2 [compile]
    ```
    
    The duplicate `dom4j:dom4j:jar:1.5.2` is gone from the tree (but is present in Maven 3.9, Maven 3.8 and all other versions).

--- maven-resolver-demos/maven-resolver-demo-snippets/src/main/java/org/apache/maven/resolver/examples/ResolveTransitiveDependencies.java
+++ maven-resolver-demos/maven-resolver-demo-snippets/src/main/java/org/apache/maven/resolver/examples/ResolveTransitiveDependencies.java
@@ -50,7 +50,10 @@ public class ResolveTransitiveDependencies {
         try (RepositorySystem system = Booter.newRepositorySystem(Booter.selectFactory(args));
                 CloseableSession session = Booter.newRepositorySystemSession(system, Booter.selectFs(args))
                         .build()) {
-            Artifact artifact = new DefaultArtifact("org.apache.maven.resolver:maven-resolver-impl:1.3.3");
+            // An "interesting" artifact that depends on itself (different version). All Maven versions
+            // released so far contained dom4j:dom4j:1.5.2 on classpath for dom4j:dom4j:1.6.1
+            // In Resolver 2.0.19+ this is fixed.
+            Artifact artifact = new DefaultArtifact("dom4j:dom4j:1.6.1");
 
             DependencyFilter classpathFilter = DependencyFilterUtils.classpathFilter(JavaScopes.COMPILE);
 
