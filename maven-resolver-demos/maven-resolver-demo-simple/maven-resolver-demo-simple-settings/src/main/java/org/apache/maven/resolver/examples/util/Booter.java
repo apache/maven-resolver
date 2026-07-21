cdc72a4586f910823b9e04f4ed32be428217c355
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2026-07-21 10:45:22 +0000

    Replace printStackTrace with proper error handling
    Closes #1999

---
+++ maven-resolver-demos/maven-resolver-demo-simple/maven-resolver-demo-simple-settings/src/main/java/org/apache/maven/resolver/examples/util/Booter.java
@@ -0,0 +1,1 @@
+fatal: path 'maven-resolver-demos/maven-resolver-demo-simple/maven-resolver-demo-simple-settings/src/main/java/org/apache/maven/resolver/examples/util/Booter.java' does not exist in '85c561c2'
---
+++ maven-resolver-test-util/src/main/java/org/eclipse/aether/DefaultRepositoryCacheTest.java
@@ -0,0 +1,1 @@
+fatal: path 'maven-resolver-test-util/src/main/java/org/eclipse/aether/DefaultRepositoryCacheTest.java' does not exist in '85c561c2'
